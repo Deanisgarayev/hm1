96dd6c5e9f33e1b81009da90ca4e6294b1d24fe3
Deanisgarayev <garaevdenis2019@gmail.com>
2024-11-21 12:16:34 +0500

    initiate commit

---
+++ main.py
@@ -0,0 +1,12 @@
+completed_hm = 12
+
+wasted_hours= 1.5
+
+course  =  'Python'
+
+took_time_for_one_task = wasted_hours/completed_hm
+
+print('Количество выполненных ДЗ - ', completed_hm,
+'Количество затраченных часов - ', wasted_hours,
+'Название курса - ',course,
+'Время на одно задание - ',took_time_for_one_task )
