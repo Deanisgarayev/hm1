f7dbc38441fc0d4d4dbd7901cd951bfd650df0be
Deanisgarayev <garaevdenis2019@gmail.com>
2024-11-21 12:37:45 +0500

    hm is done

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ course  =  'Python'
 
 took_time_for_one_task = wasted_hours/completed_hm
 
-print('Количество выполненных ДЗ - ', completed_hm,
-'Количество затраченных часов - ', wasted_hours,
-'Название курса - ',course,
-'Время на одно задание - ',took_time_for_one_task )
+print('Количество выполненных ДЗ -', completed_hm,
+'Количество затраченных часов -', wasted_hours,
+'Название курса -',course,
+'Время на одно задание -',took_time_for_one_task )
